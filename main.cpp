4b6f47e9a689048a3c5b2c7f6c9833ed284c04e0
mygreentae <lpipatanangkura@gmail.com>
2024-08-05 21:03:17 -0700

    threw excpetions for user input

--- main.cpp
+++ main.cpp
@@ -3,11 +3,27 @@
 int main() {
   try {
     
-    int gameType;
-    std::cout << "Hello! Welcome to Hangman!" << std::endl;
+    int gameType = -1;
+    std::cout << "Hello! Welcome to Hangman!" << std::endl << std::endl;
     std::cout << "Game Types: [1]: words [2]: phrases [3] words and phrases" << std::endl;
-    std::cout << "Enter Game Type: ";
-    std::cin >> gameType;
+    
+    while (true) {
+        std::cout << "Enter Game Type (1-3): ";
+        std::cin >> gameType;
+
+        // make sure the user enters an integer
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+            std::cout << "Invalid input. Please enter a valid integer." << std::endl;
+        } else { // make sure the user enters a number 1-3
+            if (gameType > 3 || gameType <= 0) {
+                std::cout << "Please enter an option between 1 and 3." << std::endl;
+            } else {
+                break; 
+            }
+        }
+    }
     
     Hangman game(gameType);
     char letter;
@@ -16,9 +32,19 @@ int main() {
 
     while(!game.isOver()) {
       game.display();
-      std::cout << "Enter letter: ";
+      std::cout << "Enter letter (a-z): ";
       std::cin >> letter;
-      game.play(std::tolower(letter));
+
+      if (std::cin.fail() || std::cin.peek() != '\n') {
+        std::cin.clear(); // Clear the error flag
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Invalid input. Please enter a single letter (a-z)." << std::endl;
+      } else if (!std::isalpha(letter)) {
+            // Check if the character is a letter and is in lower case
+            std::cout << "Invalid input. Please enter a single letter (a-z)." << std::endl;
+      } else {
+          game.play(std::tolower(letter));
+      }
     }
 
     game.display();
